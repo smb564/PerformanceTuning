22abfa4ac5211c1e0a749611721cf4f2ce253e17
Supun Abeysinghe <supun.14@cse.mrt.ac.lk>
2019-05-19 12:08:31 +0530

    add only tomcat tuning automation scripts

---
+++ bayesian_opt_only_tomcat.py
@@ -0,0 +1,72 @@
+import requests
+import time
+from hyperopt import hp
+from hyperopt import tpe
+from hyperopt import Trials
+from hyperopt import fmin
+import csv
+import sys
+import os
+
+folder_name = sys.argv[1] if sys.argv[1][-1] == "/" else sys.argv[1] + "/"
+case_name = sys.argv[2]
+
+try:
+    os.makedirs(folder_name+case_name)
+except FileExistsError:
+    print("directory already exists")
+
+ru = int(sys.argv[3])
+mi = int(sys.argv[4])
+rd = int(sys.argv[5])
+tune_interval = int(sys.argv[6])
+
+data = []
+param_history = []
+test_duration = ru + mi + rd
+tuning_interval = tune_interval  # in seconds
+
+# query and get the current thread pool size (assuming fixed thread pool)
+prev_param = int(requests.get("http://192.168.32.1:8080/getparam?name=minSpareThreads").json())
+
+
+def objective(x):
+    print("Setting fixed thread pool size to " + str(x))
+    global prev_param
+    # let's make this a fixed thread pool by maintaining minSpareThreads=maxThreads
+    # we need make sure we set the params in correct order
+    # (because we can't set a lower value for maxThreads than minSpareThreads)
+
+    if int(x) > prev_param:
+        requests.put("http://192.168.32.1:8080/setparam?name=maxThreads&value=" + str(int(x)))
+        requests.put("http://192.168.32.1:8080/setparam?name=minSpareThreads&value=" + str(int(x)))
+    else:
+        requests.put("http://192.168.32.1:8080/setparam?name=minSpareThreads&value="+str(int(x)))
+        requests.put("http://192.168.32.1:8080/setparam?name=maxThreads&value="+str(int(x)))
+
+    prev_param = int(x)
+    time.sleep(tuning_interval)
+    res = requests.get("http://192.168.32.1:8080/performance?server=tomcat").json()
+    data.append(res)
+    param_history.append(int(x))
+    print("Mean response time : " + str(res[2]))
+    return float(res[2])
+
+
+space = hp.uniform('x', 20, 400)
+tpe_trials = Trials()
+tpe_best = fmin(fn=objective, space=space, algo=tpe.suggest, trials=tpe_trials, max_evals=test_duration//tuning_interval)
+
+
+with open(folder_name + case_name + "/results.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerow(["IRR", "Request Count", "Mean Latency (for window)", "99th Latency"])
+    for line in data:
+        writer.writerow(line)
+
+with open(folder_name + case_name + "/param_history.csv", "w") as f:
+    writer = csv.writer(f)
+    for line in param_history:
+        writer.writerow([line])
+
+print("Optimization complete")
---
+++ running_script_only_tomcat.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+source venv/bin/activate
+
+optimizer="gp_optimizer.py"
+
+declare -A MIX2NAME
+MIX2NAME=( ["1"]="browsing" ["2"]="shopping" ["3"]="ordering")
+
+RU="60"
+MI="600"
+RD="60"
+
+# Parameters are tuned this often
+TUNING_INTERVAL="60"
+
+# Interval in which performance is measured
+MEASURING_INTERVAL="20"
+
+PARENT_FOLDER="only_tomcat_gpopt"
+
+if [[ -d "${PARENT_FOLDER}" ]]
+then
+    read -p "Parent directory already exists. Replace? (Y/n)" yn
+    case $yn in
+        [Yy]* ) true;;
+        * ) exit;;
+    esac
+fi
+
+for MIX in 1 2 3
+do
+    for CONCURRENCY in 100 200 300 400
+    do
+        FOLDER_NAME="${PARENT_FOLDER}/${MIX2NAME[${MIX}]}_${CONCURRENCY}"
+
+        # create the directory at client side
+        ssh wso2@192.168.32.6 "cd supun/dist && mkdir -p $FOLDER_NAME"
+
+
+        CASE_NAME="tuning"
+        echo "Running the tuning case"
+
+        echo "Restarting tomcat server..."
+        # restart the tomcat server
+        ssh wso2@192.168.32.11 "./supun/scripts/restart-tomcat.sh"
+
+        echo "Tomcat restarted"
+
+        echo "Reconnecting Performance Monitor to Tomcat"
+        # reconnect the monitor server to the new Tomcat instance
+        curl 192.168.32.1:8080/reconnect
+
+        # just in case the monitor takes some to connect
+        sleep 3s
+
+        nohup python3 server_side_metrics.py "$FOLDER_NAME" "$CASE_NAME" "$RU" "$MI" "$RD" "$MEASURING_INTERVAL"> metrics_log.txt &
+        nohup python3 ${optimizer} "$FOLDER_NAME" "$CASE_NAME" "$RU" "$MI" "$RD" "$TUNING_INTERVAL"> optimizer.log &
+
+        # run the performance test
+        ssh wso2@192.168.32.6 "cd supun/dist && java rbe.RBE -EB rbe.EBTPCW2Factory $CONCURRENCY -OUT $FOLDER_NAME/$CASE_NAME.m -RU $RU -MI $MI -RD $RD -ITEM 1000 -TT 0.1 -MAXERROR 0 -WWW http://192.168.32.11:8080/tpcw/"
+
+
+        # running the none tuning case
+        CASE_NAME="default"
+        echo "Running the default case without tuning"
+
+        echo "Restarting tomcat server..."
+        # restart the tomcat server
+        ssh wso2@192.168.32.11 "./supun/scripts/restart-tomcat.sh"
+
+        echo "Tomcat restarted"
+
+        echo "Reconnecting Performance Monitor to Tomcat"
+        # reconnect the monitor server to the new Tomcat instance
+        curl 192.168.32.1:8080/reconnect
+
+        # just in case the monitor takes some to connect
+        sleep 3s
+
+        nohup python3 server_side_metrics.py "$FOLDER_NAME" "$CASE_NAME" "$RU" "$MI" "$RD" "$MEASURING_INTERVAL"> metrics_log.txt &
+
+        # run the performance test
+        ssh wso2@192.168.32.6 "cd supun/dist && java rbe.RBE -EB rbe.EBTPCW2Factory $CONCURRENCY -OUT $FOLDER_NAME/$CASE_NAME.m -RU $RU -MI $MI -RD $RD -ITEM 1000 -TT 0.1 -MAXERROR 0 -WWW http://192.168.32.11:8080/tpcw/"
+
+        # now join the plots
+        python3 join_plots.py ${FOLDER_NAME} "default" "tuning"
+    done
+done
