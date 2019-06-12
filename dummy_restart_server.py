8bc425b3df109526f4393ef98811588e3d00f3c9
Supun Abeysinghe <supun.14@cse.mrt.ac.lk>
2019-06-12 10:58:35 +0530

    fix dummy restart

--- dummy_restart_server.py
+++ dummy_restart_server.py
@@ -5,12 +5,6 @@ import os
 
 folder_name = sys.argv[1] if sys.argv[1][-1] == "/" else sys.argv[1] + "/"
 case_name = sys.argv[2]
-only_tomcat = True
-
-try:
-    os.makedirs(folder_name+case_name)
-except FileExistsError:
-    print("directory already exists")
 
 ru = int(sys.argv[3])
 mi = int(sys.argv[4])
@@ -26,4 +20,3 @@ num_iter = test_duration // tuning_interval
 for i in range(num_iter):
     requests.get("http://192.168.32.10:5001/reload")
     time.sleep(tuning_interval)
-
