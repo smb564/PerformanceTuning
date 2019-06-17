33931f09a7c42a77885569478bb9ffffbb56684c
Supun Abeysinghe <supun.14@cse.mrt.ac.lk>
2019-06-17 14:23:18 +0530

    add updated running script for only tomcat case

--- running_script_only_tomcat.sh
+++ running_script_only_tomcat.sh
@@ -1,23 +1,29 @@
 #!/usr/bin/env bash
 source venv/bin/activate
 
-optimizer="bayesian_opt_only_tomcat.py"
+optimizer="skopt_gp_only_tomcat.py"
 
 declare -A MIX2NAME
 MIX2NAME=( ["1"]="browsing" ["2"]="shopping" ["3"]="ordering")
 
+declare -A MIX2CONCURRENCY
+MIX2CONCURRENCY=( ["1"]="800" ["2"]="600" ["3"]="100")
+
 RU="60"
-MI="600"
+MI="3600"
 RD="60"
 URL="http://192.168.32.11:8080"
 
 # Parameters are tuned this often
-TUNING_INTERVAL="60"
+TUNING_INTERVAL="120"
+
+# Time window to take average response time
+MEASURING_WINDOW="60"
 
 # Interval in which performance is measured
-MEASURING_INTERVAL="20"
+MEASURING_INTERVAL="60"
 
-PARENT_FOLDER="only_tomcat_tpe"
+PARENT_FOLDER="only_tomcat/long_tt_1"
 
 if [[ -d "${PARENT_FOLDER}" ]]
 then
@@ -32,73 +38,77 @@ mkdir -p ${PARENT_FOLDER}
 
 for MIX in 1 2 3
 do
-    for CONCURRENCY in 100 200 300 400
-    do
-        FOLDER_NAME="${PARENT_FOLDER}/${MIX2NAME[${MIX}]}_${CONCURRENCY}"
+    CONCURRENCY=${MIX2CONCURRENCY[${MIX}]}
+    FOLDER_NAME="${PARENT_FOLDER}/${MIX2NAME[${MIX}]}_${CONCURRENCY}"
 
-        # create the directory at client side
-        ssh wso2@192.168.32.6 "cd supun/dist && mkdir -p $FOLDER_NAME"
+    # create the directory at client side
+    ssh wso2@192.168.32.6 "cd supun/dist && mkdir -p $FOLDER_NAME"
+
+    CASE_NAME="tuning"
+    echo "Running the tuning case"
+
+    echo "Restarting tomcat server..."
+    # restart the tomcat server
+    ssh wso2@192.168.32.11 "./supun/scripts/restart-tomcat.sh"
+
+    echo "Tomcat restarted"
+
+    # start the client
+    nohup ssh wso2@192.168.32.6 "cd supun/dist && java -Dcom.sun.management.jmxremote " \
+    "-Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.local.only=false " \
+    "-Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false " \
+    "-jar rbe.jar -WINDOW ${MEASURING_WINDOW} -EB rbe.EBTPCW${MIX}Factory $CONCURRENCY -OUT $FOLDER_NAME/$CASE_NAME.m -RU $RU -MI $MI -RD $RD " \
+    "-ITEM 1000 -TT 1 -MAXERROR 0 -WWW ${URL}/tpcw/" > eb.log &
+
+    sleep ${RU}s
+
+    echo "Reconnecting Performance Monitor to Tomcat"
+    # reconnect the monitor server to the new Tomcat instance
+    curl 192.168.32.2:8080/reconnect
+
+    nohup python3 client_side_metrics.py "$FOLDER_NAME" "$CASE_NAME" "0" "$MI" "0" "$MEASURING_INTERVAL" "${MEASURING_WINDOW}"> client_side.txt &
+    nohup python3 ${optimizer} "$FOLDER_NAME" "$CASE_NAME" "0" "$MI" "0" "$TUNING_INTERVAL"> optimizer.log &
+
+    # to finish the tests after the time eliminates
+    sleep ${MI}s
+    sleep ${RD}s
+    sleep 100s
+
+    # reset the database
+    ssh wso2@192.168.32.11 "mysql -u root -h 192.168.32.7 -pjavawso2 < drop_and_create.sql"
+    ssh wso2@192.168.32.11 "mysql -u root -h 192.168.32.7 -pjavawso2 tpcw < tpcw-dump.sql"
+
+    # running the none tuning case
+    CASE_NAME="default"
+    echo "Running the default case without tuning"
+
+    echo "Restarting tomcat server..."
+    # restart the tomcat server
+    ssh wso2@192.168.32.11 "./supun/scripts/restart-tomcat-long-delay.sh"
+
+    echo "Tomcat restarted"
+
+    # start the client
+    nohup ssh wso2@192.168.32.6 "cd supun/dist && java -Dcom.sun.management.jmxremote " \
+    "-Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.local.only=false " \
+    "-Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false " \
+    "-jar rbe.jar -WINDOW ${MEASURING_WINDOW} -EB rbe.EBTPCW${MIX}Factory $CONCURRENCY -OUT $FOLDER_NAME/$CASE_NAME.m -RU $RU -MI $MI -RD " \
+    "$RD -ITEM 1000 -TT 1 -MAXERROR 0 -WWW ${URL}/tpcw/" > eb.log &
+
+    sleep ${RU}s
+    nohup python3 client_side_metrics.py "$FOLDER_NAME" "$CASE_NAME" "0" "$MI" "0" "$MEASURING_INTERVAL" "${MEASURING_WINDOW}"> client_side.txt &
+
+    # to finish the tests after the time eliminates
+    sleep ${MI}s
+    sleep ${RD}s
+    sleep 100s
 
 
-        CASE_NAME="tuning"
-        echo "Running the tuning case"
+    # now join the plots
+    python3 join_plots.py ${FOLDER_NAME} "default" "tuning"
 
-        echo "Restarting tomcat server..."
-        # restart the tomcat server
-        ssh wso2@192.168.32.11 "./supun/scripts/restart-tomcat.sh"
+    # reset the database
+    ssh wso2@192.168.32.11 "mysql -u root -h 192.168.32.7 -pjavawso2 < drop_and_create.sql"
+    ssh wso2@192.168.32.11 "mysql -u root -h 192.168.32.7 -pjavawso2 tpcw < tpcw-dump.sql"
 
-        echo "Tomcat restarted"
-
-        echo "Reconnecting Performance Monitor to Tomcat"
-        # reconnect the monitor server to the new Tomcat instance
-        curl 192.168.32.2:8080/reconnect
-
-        # just in case the monitor takes some to connect
-        sleep 3s
-
-        nohup python3 server_side_metrics.py "$FOLDER_NAME" "$CASE_NAME" "$RU" "$MI" "$RD" "$MEASURING_INTERVAL"> metrics_log.txt &
-        nohup python3 ${optimizer} "$FOLDER_NAME" "$CASE_NAME" "$RU" "$MI" "$RD" "$TUNING_INTERVAL"> optimizer.log &
-
-        # run the performance test
-        nohup ssh wso2@192.168.32.6 "cd supun/dist && java rbe.RBE -EB rbe.EBTPCW${MIX}Factory $CONCURRENCY -OUT $FOLDER_NAME/$CASE_NAME.m -RU $RU -MI $MI -RD $RD -ITEM 1000 -TT 0.1 -MAXERROR 0 -WWW ${URL}/tpcw/" > eb.log &
-
-        # to finish the tests after the time eliminates
-        sleep ${RU}s
-        sleep ${MI}s
-        sleep ${RD}s
-        sleep 100s
-        ssh wso2@192.168.32.11 "./supun/scripts/restart-tomcat.sh"
-
-        # running the none tuning case
-        CASE_NAME="default"
-        echo "Running the default case without tuning"
-
-        echo "Restarting tomcat server..."
-        # restart the tomcat server
-        ssh wso2@192.168.32.11 "./supun/scripts/restart-tomcat.sh"
-
-        echo "Tomcat restarted"
-
-        echo "Reconnecting Performance Monitor to Tomcat"
-        # reconnect the monitor server to the new Tomcat instance
-        curl 192.168.32.2:8080/reconnect
-
-        # just in case the monitor takes some to connect
-        sleep 3s
-
-        nohup python3 server_side_metrics.py "$FOLDER_NAME" "$CASE_NAME" "$RU" "$MI" "$RD" "$MEASURING_INTERVAL"> metrics_log.txt &
-
-        # run the performance test
-        nohup ssh wso2@192.168.32.6 "cd supun/dist && java rbe.RBE -EB rbe.EBTPCW${MIX}Factory $CONCURRENCY -OUT $FOLDER_NAME/$CASE_NAME.m -RU $RU -MI $MI -RD $RD -ITEM 1000 -TT 0.1 -MAXERROR 0 -WWW ${URL}/tpcw/" > eb.log &
-
-        # to finish the tests after the time eliminates
-        sleep ${RU}s
-        sleep ${MI}s
-        sleep ${RD}s
-        sleep 100s
-        ssh wso2@192.168.32.11 "./supun/scripts/restart-tomcat.sh"
-
-        # now join the plots
-        python3 join_plots.py ${FOLDER_NAME} "default" "tuning"
-    done
 done
